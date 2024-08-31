ac4669d23e339b0ebab940f0163df3e682a0898b
rbo13 <chardyy.orofeo@gmail.com>
2024-08-31 21:36:41 +0800

    added failing test for GameHandler class

--- src/test/java/rbo13/github/minesweeper/game/GameHandlerTest.java
+++ src/test/java/rbo13/github/minesweeper/game/GameHandlerTest.java
@@ -1,4 +1,38 @@
 package rbo13.github.minesweeper.game;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import rbo13.github.minesweeper.util.Cell;
+import rbo13.github.minesweeper.util.Position;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 public class GameHandlerTest {
+
+    private Minefield minefield;
+    private GameHandler gameHandler;
+
+    @BeforeEach
+    void setup() {
+        minefield = Mockito.mock(Minefield.class);
+        gameHandler = new GameHandler(minefield);
+    }
+
+    @Test
+    void testRevealCell_RevealCellAndContinue() {
+        Position position = new Position(1, 1);
+        Cell cell = Mockito.mock(Cell.class);
+        when(minefield.isValidPosition(position.row(), position.col())).thenReturn(true);
+        when(minefield.getCell(position.row(), position.col())).thenReturn(cell);
+        when(cell.isRevealed()).thenReturn(false);
+        when(cell.isMine()).thenReturn(false);
+        when(cell.getAdjacentMines()).thenReturn(1);
+
+        gameHandler.revealCell(position);
+        verify(cell).setRevealed(true);
+        assertFalse(gameHandler.isGameOver());
+    }
 }
