1e38b7755a678b981f58360167171bf798028024
rbo13 <chardyy.orofeo@gmail.com>
2024-08-31 13:29:53 +0800

    added failing test

--- src/test/java/rbo13/github/minesweeper/util/CellTest.java
+++ src/test/java/rbo13/github/minesweeper/util/CellTest.java
@@ -1,4 +1,16 @@
 package rbo13.github.minesweeper.util;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 class CellTest {
+
+
+    @Test
+    void testCellInitialization() {
+        Cell cell = new Cell();
+        assertNotNull(cell);
+    }
 }
