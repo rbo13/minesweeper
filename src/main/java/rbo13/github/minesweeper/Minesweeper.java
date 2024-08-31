c17fc62046e7102aa2a6615be161a00b2bec567b
rbo13 <chardyy.orofeo@gmail.com>
2024-08-31 22:53:47 +0800

    added new Minesweeper class to represent the Minesweeper game

---
+++ src/main/java/rbo13/github/minesweeper/Minesweeper.java
@@ -0,0 +1,12 @@
+package rbo13.github.minesweeper;
+
+import rbo13.github.minesweeper.game.Game;
+
+public class Minesweeper implements Game {
+
+
+    @Override
+    public void play() {
+
+    }
+}
