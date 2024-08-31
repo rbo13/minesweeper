81620e30eeb6f91a407c2f0c33ce2cd2d73ec19d
rbo13 <chardyy.orofeo@gmail.com>
2024-08-31 13:23:19 +0800

    added test file for Cell class that represents a basic cell in a minefield

--- src/main/java/rbo13/github/minesweeper/App.java
+++ src/main/java/rbo13/github/minesweeper/App.java
@@ -1,9 +1,5 @@
 package rbo13.github.minesweeper;
 
-/**
- * Hello world!
- *
- */
 public class App
 {
     public static void main( String[] args )
---
+++ src/test/java/rbo13/github/minesweeper/util/CellTest.java
@@ -0,0 +1,4 @@
+package rbo13.github.minesweeper.util;
+
+class CellTest {
+}
