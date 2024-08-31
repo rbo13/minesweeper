b3382eb13e16927c3068ee92af3a07362d61852b
rbo13 <chardyy.orofeo@gmail.com>
2024-08-31 13:03:41 +0800

    initial commit using maven as build tool

---
+++ src/main/java/rbo13/github/minesweeper/App.java
@@ -0,0 +1,13 @@
+package rbo13.github.minesweeper;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
