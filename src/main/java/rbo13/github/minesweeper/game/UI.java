a8a5bbab49d2a808e7a5b5f6f15601cdc58c6232
rbo13 <chardyy.orofeo@gmail.com>
2024-08-31 22:50:53 +0800

    refactor: added passing test for displaying grid

--- src/main/java/rbo13/github/minesweeper/game/UI.java
+++ src/main/java/rbo13/github/minesweeper/game/UI.java
@@ -1,5 +1,7 @@
 package rbo13.github.minesweeper.game;
 
+import rbo13.github.minesweeper.util.Cell;
+
 import java.util.Scanner;
 
 public class UI {
@@ -24,6 +26,7 @@ public class UI {
 
     private final Minefield minefield;
     private final Scanner scanner;
+    private static final String MINE = "X ";
     private static final double MAX_MINE_PERCENTAGE = 0.35;
 
     public UI(Minefield minefield) {
@@ -32,4 +35,29 @@ public class UI {
     }
 
     public void displayWelcomeMessage() { System.out.println("Welcome to Minesweeper!"); }
+
+    public void displayGrid(boolean revealAll) {
+        System.out.print("  ");
+        for (int i = 0; i < minefield.getSize(); i++) {
+            System.out.print((i + 1) + " ");
+        }
+        System.out.println();
+
+        for (int i = 0; i < minefield.getSize(); i++) {
+            System.out.print((char) ('A' + i) + " ");
+            for (int j = 0; j < minefield.getSize(); j++) {
+                Cell cell = minefield.getCell(i, j);
+                if (revealAll || cell.isRevealed()) {
+                    if (cell.isMine()) {
+                        System.out.print(MINE);
+                    } else {
+                        System.out.print(cell.getAdjacentMines() + " ");
+                    }
+                } else {
+                    System.out.print("_ ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
--- src/test/java/rbo13/github/minesweeper/game/UITest.java
+++ src/test/java/rbo13/github/minesweeper/game/UITest.java
@@ -1,5 +1,6 @@
 package rbo13.github.minesweeper.game;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,8 +12,7 @@ import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class UITest {
@@ -20,32 +20,61 @@ public class UITest {
     private UI ui;
     private Minefield minefield;
     private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-    private final PrintStream printStream = System.out;
 
     @BeforeEach
     void setup() {
+        System.setOut(new PrintStream(outputStream));
         minefield = mock(Minefield.class);
         ui = new UI(minefield);
-        System.setOut(new PrintStream(outputStream));
     }
 
     @Test
     void testDisplayWelcomeMessage() {
         ui.displayWelcomeMessage();
-        assertEquals("Welcome to Minesweeper!\r\n", outputStream.toString());
+        String expectedOutput = "Welcome to Minesweeper!\n";
+        assertEquals(normalizeLineEndings(expectedOutput), normalizeLineEndings(outputStream.toString()));
     }
 
     @Test
-    void testDiplayGrid() {
-        when(minefield.getCell(anyInt(), anyInt())).thenReturn(mock(Cell.class));
-        ui.displayGrid(false);
-        String expected = """
-                  1 2 3 4\s
-                A * * * *\s
-                B * * * *\s
-                C * * * *\s
-                D * * * *\s
-                """;
-        assertEquals(expected, outputStream.toString());
+    void testDisplayGrid_RevealAll() {
+        lenient().when(minefield.getSize()).thenReturn(2); // Example: 2x2 grid
+
+        Cell cell1 = mock(Cell.class);
+        Cell cell2 = mock(Cell.class);
+        Cell cell3 = mock(Cell.class);
+        Cell cell4 = mock(Cell.class);
+
+        lenient().when(cell1.isMine()).thenReturn(false);
+        lenient().when(cell1.isRevealed()).thenReturn(true);
+        lenient().when(cell1.getAdjacentMines()).thenReturn(1);
+
+        lenient().when(cell2.isMine()).thenReturn(true);
+        lenient().when(cell2.isRevealed()).thenReturn(true);
+
+        lenient().when(cell3.isMine()).thenReturn(false);
+        lenient().when(cell3.isRevealed()).thenReturn(true);
+        lenient().when(cell3.getAdjacentMines()).thenReturn(0);
+
+        lenient().when(cell4.isMine()).thenReturn(false);
+        lenient().when(cell4.isRevealed()).thenReturn(true);
+        lenient().when(cell4.getAdjacentMines()).thenReturn(2);
+
+        lenient().when(minefield.getCell(0, 0)).thenReturn(cell1);
+        lenient().when(minefield.getCell(0, 1)).thenReturn(cell2);
+        lenient().when(minefield.getCell(1, 0)).thenReturn(cell3);
+        lenient().when(minefield.getCell(1, 1)).thenReturn(cell4);
+
+        ui.displayGrid(true);
+
+        String expectedOutput = "  1 2 \n" +  // Header row
+                "A 1 X \n" +  // Row A
+                "B 0 2 \n";   // Row B
+
+        assertEquals(normalizeLineEndings(expectedOutput), normalizeLineEndings(outputStream.toString()));
+    }
+
+    // fix line endings
+    private String normalizeLineEndings(String input) {
+        return input.replace("\r\n", "\n").replace("\r", "\n");
     }
 }
